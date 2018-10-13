4d2b6a9cf5dd822460e570b3ac96b2304c9fb2a9
sne007 <challasnehith@gmail.com>
2018-10-13 00:38:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chall/database_theory/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.chall.database_theory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
